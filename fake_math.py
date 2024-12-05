a940b9a4f63fc1ac147f6f4059c752d34d74bfeb
dmitrius77 <77dmitrius7777@gmail.com>
2024-12-05 22:39:30 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+       return 'Ошибка'
+    return first / second
+
+#print(divide(4, 0))
---
+++ true_math.py
@@ -0,0 +1,13 @@
+import math
+def divide(first, second):
+    if second == 0:
+       return math.inf
+    else:
+        return first / second
+
+
+#print(divide(69, 3))
+
+
+
+
